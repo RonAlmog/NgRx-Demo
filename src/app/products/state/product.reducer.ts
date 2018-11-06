414cd202262908f02ed0d6b8b5f8fb8bf9fda77c
Ron Almog <ron8132@gmail.com>
2018-11-06 16:49:25 -0500

    extending the state interface

--- src/app/products/state/product.reducer.ts
+++ src/app/products/state/product.reducer.ts
@@ -1,9 +1,20 @@
+import { Product } from '../product';
+import * as fromRoot from '../../state/app.state';
+
+export interface State extends fromRoot.State {
+  products: ProductState;
+}
+
+export interface ProductState {
+  showProductCode: boolean;
+  currentProduct: Product;
+  products: Product[];
+}
+
 export function reducer(state, action) {
   switch (action.type) {
 
     case 'TOGGLE_PRODUCT_CODE':
-      // console.log('existing state: ', JSON.stringify(state));
-      // console.log('payload: ', action.payload);
       return {
         ...state,
         showProductCode: action.payload
